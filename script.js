fd9c6ccd01e047be70128dc5ed85011f9a6e7d73
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-18 14:09:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let N1 = prompt("Digite o valor do primeiro lado do triângulo.")
+let N2 = prompt("Digite o valor do segundo lado do triângulo.")
+let N3 = prompt("Digite o valor do terceiro lado do triângulo.")
+var Classificação
+if(N1 == N2 == N3){
+    console.log("O triângulo é Equilátero")
+}
+else if(N1 == N2 == N3){
+    console.log("O triângulo é Equilátero")
+}
